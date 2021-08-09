6e6e844d5d125e2821eabe51598f738e303454cb
インテンス <intense@intensunoiMac.local>
2021-08-09 18:32:54 +0900

    new upload

--- pages/index.vue
+++ pages/index.vue
@@ -14,19 +14,16 @@ export default {
   async asyncData(params) {
     const page = params.p || '1'
     const limit = 10
-    const { data } = await axios.get(
-<<<<<<< HEAD
-      
+    const { data } = await axios.get(      
       // add ?limit-${limit}&offset=${(page - 1)* limit}
       'https://i-columnblog.microcms.io/api/v1/column?limit=${limit}&offset=${(page - 1) * limit}',
       {
         
         headers: { 'X-API-KEY': '8af4ace2-1340-419c-9101-f57ca02a6ec9' }
       }
-=======
+
       `https://i-column.microcms.io/api/v1/posts?limit=${limit}&offset=${(page - 1) * limit}`,
       { headers: { 'X-API-KEY': 'c1531420-9002-48fe-a986-5671f608cd2c' } }
->>>>>>> 4bfa76b9d85f577c42af9b0d6b6658ce06e907f6
     )
     return data
   }
