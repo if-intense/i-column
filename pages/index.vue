4bfa76b9d85f577c42af9b0d6b6658ce06e907f6
Ryo Fujiwara <jjcproject@icloud.com>
2021-08-09 17:38:37 +0900

    check

--- pages/index.vue
+++ pages/index.vue
@@ -12,19 +12,11 @@
 import axios from 'axios'
 export default {
   async asyncData(params) {
-
-    // adding for pagination
-    const page = params.p|| '1'
+    const page = params.p || '1'
     const limit = 10
-
     const { data } = await axios.get(
-      
-      // add ?limit-${limit}&offset=${(page - 1)* limit}
-      'https://icolumn.microcms.io/api/v1/posts?limit=${limit}&offset=${(page - 1) * limit}',
-      {
-        
-        headers: { 'X-API-KEY': 'c1531420-9002-48fe-a986-5671f608cd2c' }
-      }
+      `https://i-column.microcms.io/api/v1/posts?limit=${limit}&offset=${(page - 1) * limit}`,
+      { headers: { 'X-API-KEY': 'c1531420-9002-48fe-a986-5671f608cd2c' } }
     )
     return data
   }
